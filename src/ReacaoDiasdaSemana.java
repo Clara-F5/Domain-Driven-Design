5637b0a644fe3d461db024d715983caa6da42189
Clara-F5 <73361499+Clara-F5@users.noreply.github.com>
2023-09-11 09:29:47 -0300

    Aula do dia 11/09 - IF...ELSE

---
+++ src/ReacaoDiasdaSemana.java
@@ -0,0 +1,36 @@
+class ReacaoDiasdaSemana{
+
+public static void main(String[] args){
+
+String dia = args[0];
+
+switch(dia){
+
+case "segunda":
+	System.out.println("Ainda é segunda");
+	break;
+case "terca":
+	System.out.println("Ainda é terça");
+	break;
+case "quarta":
+	System.out.println("Quarta...");
+	break;
+case "quinta":
+	System.out.println("Quase fds");
+	break;
+case "sexta":
+	System.out.println("Quase fds");
+	break;
+case "sabado":
+	System.out.println("FDS!!");
+	break;
+case "domingo":
+	System.out.println("FDS!!");
+	break;
+	
+default: 
+	System.out.println("Dia inválido");
+}
+
+}
+}
