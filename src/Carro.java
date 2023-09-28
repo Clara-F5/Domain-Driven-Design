2c187f18cf894f147d5956f973fb14bf9565ff46
Clara Fonte <clara00017@hotmail.com>
2023-09-28 13:20:06 -0300

    Aula 28/09 - CLASSES

---
+++ src/Carro.java
@@ -0,0 +1,85 @@
+class TesteCarro {
+
+    public static void main(String[] args) {
+
+        Carro carrinho = new Carro("fastback", "fiat", "branco", true);
+
+        carrinho.descreverCarro();
+
+        carrinho.ligarOuDesligar();
+
+        carrinho.velocidade = 5;
+
+        carrinho.acelerar();
+
+        carrinho.descreverCarro();
+
+        carrinho.freiar();
+
+        carrinho.mudarMarcha(-1);
+
+
+        carrinho.descreverCarro();
+
+
+    }
+}
+
+
+public class Carro {
+
+    String modelo;
+    String marca;
+    String cor;
+    boolean automatico;
+    int marchaAtual = 0;
+    double velocidade = 0;
+    boolean ligado = false;
+
+ Carro(String modelo, String marca, String cor, boolean automatico){
+     this.modelo = modelo;
+     this.marca = marca;
+     this.cor = cor;
+     this.automatico = automatico;
+    }
+
+    void acelerar() {
+        if (ligado) {
+            velocidade += 1;
+        }
+    }
+     void freiar(){
+         if(ligado){
+             velocidade -=1;
+         }
+        }
+
+     void descreverCarro(){
+         System.out.println("Atributos do carro");
+         System.out.println("marca: "+marca);
+         System.out.println("modelo:"+modelo);
+         System.out.println("cor: "+cor);
+         System.out.println("automatico: "+automatico);
+         System.out.println("velocidade:"+velocidade);
+         System.out.println("marcha atual:"+marchaAtual);
+         System.out.println("ligado:"+ligado);
+     }
+
+    public boolean ligarOuDesligar() {
+        ligado = !ligado;
+        return ligado;
+
+ }
+
+    public boolean mudarMarcha(int marcha) {
+        if (marcha < -1 || marcha > 5) {
+            return false;
+        }
+        if (marcha == -1 && marcha != 0){
+            return false;
+        }
+        marchaAtual = marcha;
+        return true;
+    }
+}
+
---
+++ src/Pessoa.java
@@ -0,0 +1,54 @@
+class TestePessoa{
+
+    public static void main(String[] args){
+
+        Pessoa clara = new Pessoa("Clara", "Fonte", 63000, 153 );
+
+        clara.getNomeCompleto();
+        clara.getAlturaEmMetros();
+        clara.getPesoEmQuilogramas();
+
+        clara.descreverPessoa();
+
+    }
+}
+
+
+
+public class Pessoa {
+
+    String nome;
+    String sobrenome;
+    int pesoEmGramas;
+    int alturaemCentimetros;
+
+    Pessoa() {
+
+    }
+
+    Pessoa(String nome, String sobrenome, int pesoEmGramas, int alturaemCentimetros){
+
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.pesoEmGramas = pesoEmGramas;
+        this.alturaemCentimetros = alturaemCentimetros;
+    }
+
+    void descreverPessoa(){
+        System.out.println("Descrição da Pessoa");
+        System.out.println("nome completo:"+getNomeCompleto());
+        System.out.println("altura:"+getAlturaEmMetros());
+        System.out.println("peso:"+getPesoEmQuilogramas());
+    }
+    public String getNomeCompleto() {
+        return nome + " " +sobrenome;
+    }
+
+    public double getPesoEmQuilogramas(){
+        return (double) pesoEmGramas /1000;
+    }
+
+    public double getAlturaEmMetros(){
+        return (double) alturaemCentimetros/100;
+    }
+}
