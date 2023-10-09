56f1ff336a1d2429f55e123f93388082c9b3153f
logonrmlocal <clara00017@hotmail.com>
2023-10-09 09:47:56 -0300

    aula 9/10 - pacotes

---
+++ src/ExemplosClassesJavaLang.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ExemplosClassesJavaLang {
+    public static void main(String[] args) {
+        System.out.println(String.class);
+        System.out.println(Integer.class);
+        System.out.println(Double.class);
+        System.out.println(System.class);
+        System.out.println(Scanner.class);
+        System.out.println(java.util.ArrayList.class);
+        System.out.println(ExemplosClassesJavaLang.class);
+
+    }
+}
---
+++ src/br/com/fiap/exemplo/MinhaClasseComPacote.java
@@ -0,0 +1,7 @@
+
+package br.com.fiap.exemplo;
+public class MinhaClasseComPacote {
+    public static void main(String[] args) {
+        System.out.println("Olá da minha classe com pacote");
+    }
+}
